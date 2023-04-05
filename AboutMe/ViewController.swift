10d3297d62664b1019a65214f20d4ef2a5d0db56
Zaki <menza.zm@gmail.com>
2023-04-05 16:46:12 +0600

    First part interface details are setted

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var userNameLabel: UITextField!
+    
+    @IBOutlet var passwordLabel: UITextField!
+    
+    
+    @IBOutlet var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func loginButtonTapped() {
+    }
+    
+    
+    
+    @IBAction func hintAlertButtonsTapped(_ sender: UIButton) {
+    }
+    
+    
+    
 }
 
